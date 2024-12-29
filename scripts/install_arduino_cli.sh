df0e8143fbd50769881cb3a50ae727f16242159f
Konstantinos Papaspyridis <k.papaspyridis@mail.utoronto.ca>
2024-12-29 18:58:35 +0200

    Fix unpacking through xz

--- scripts/install_arduino_cli.sh
+++ scripts/install_arduino_cli.sh
@@ -35,7 +35,7 @@ echo "Installing arduino:mbed_nano core"
 /tmp/bin/arduino-cli core install arduino:mbed_nano
 
 echo "Unpacking toolchain"
-tar -xzf "14_2.tar.xz"
+tar -xJf "14_2.tar.xz"
 mkdir -p "${HOME}/.arduino15/packages/arduino/tools/arm-none-eabi-gcc/14_2"
 mv "arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi" "${HOME}/.arduino15/packages/arduino/tools/arm-none-eabi-gcc/14_2"
 rm -f *.tar.xz *.gz
